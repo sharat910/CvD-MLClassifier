818493e0239f99c7eb888866dc1a459db4689b76
sharat910 <sharat910@gmail.com>
2017-03-13 15:04:25 +0530

    Done

---
+++ readAndPreprocessImage.m
@@ -0,0 +1,19 @@
+% Read and pre-process images
+% Copyright 2016 The MathWorks, Inc.
+function Iout = readAndPreprocessImage(filename)
+
+I = imread(filename);
+
+% Some images may be grayscale. Replicate the image 3 times to
+% create an RGB image.
+if ismatrix(I)
+    I = cat(3,I,I,I);
+end
+
+% Resize the image as required for the CNN.
+Iout = imresize(I, [227 227]);
+
+%Applying Gaussian Filter on the image (Pre-processing it)
+Iout = imgaussfilt(Iout, 1);
+
+end
